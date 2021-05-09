84f8614f57d197418da8e186a4d1b67ca39e3146
遥奕 <jibing.yjb@alibaba-inc.com>
2021-05-09 17:11:48 +0800

    2-addTwoNumbers

--- src/main/java/org/alibaba/2-AddTwoNumbers.java
+++ src/main/java/org/alibaba/2-AddTwoNumbers.java
@@ -10,21 +10,26 @@ package org.alibaba;
 
 class addTwoNumbers {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode initList = null;
         ListNode ret = null;
-        while (l1 != null || l2 != null) {
+        int ford = 0;
+        while (l1 != null || l2 != null || ford != 0) {
             int s1 = l1 == null ? 0 : l1.val;
             int s2 = l2 == null ? 0 : l2.val;
-            int cur = (s1 + s2) % 10;
-            int ford = (s1 + s2) / 10;
+            int cur = (s1 + s2 + ford) % 10;
+            ford = (s1 + s2 + ford) / 10;
 
-            if(ret == null) {
-                ret = new ListNode(cur);
-            }else{
-                ret.next = new ListNode(cur);
-                ret = ret.next;
+            if (initList == null) {
+                initList = new ListNode(cur);
+                ret = initList;
+            } else {
+                initList.next = new ListNode(cur);
+                initList = initList.next;
             }
-            l1 = l1.next == null? null:l1.next;
-            l2 = l2.next == null? null:l2.next;
+            if (l1 != null)
+                l1 = l1.next == null ? null : l1.next;
+            if (l2 != null)
+                l2 = l2.next == null ? null : l2.next;
         }
         return ret;
     }
